2a82f45d6fd96010e92b62b8487b5cf679b305df
Ralph Andre Garcia <146960843+JudgementKazzy2023@users.noreply.github.com>
2025-01-22 15:27:42 +0800

    Add files via upload

---
+++ ColorSwitch.js
@@ -0,0 +1,16 @@
+function toggleTheme() {
+    const html = document.documentElement;
+    const currentTheme = html.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    
+    html.setAttribute('data-theme', newTheme);
+    
+   
+    localStorage.setItem('theme', newTheme);
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    document.documentElement.setAttribute('data-theme', savedTheme);
+});
